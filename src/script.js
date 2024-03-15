afc33f1089fd040b1295a2df7bf223fe5067dd4f
Alejandro Erickson <aerickson@copperleaf.com>
2024-03-14 22:07:58 -0700

    WIP

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import './style.scss';
+
 document.addEventListener('DOMContentLoaded', () => {
     const cardsContainer = document.getElementById('cardsContainer');
     const checkButton = document.getElementById('checkButton');
@@ -12,24 +14,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Render cards
     cards.forEach((card, index) => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('card');
-        cardElement.dataset.index = index;
+        const cardWrapper = document.createElement('div');
+        cardWrapper.classList.add('card-wrapper');
 
-        // Add shapes based on the card vector
-        cardElement.innerHTML = card.map((bit, i) => bit ? shapes[i] : '').join('');
-        cardsContainer.appendChild(cardElement);
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `card-${index}`;
+        checkbox.classList.add('card-checkbox');
 
-        cardElement.addEventListener('click', () => {
-            cardElement.classList.toggle('selected');
-        });
+        const label = document.createElement('label');
+        label.htmlFor = checkbox.id;
+        label.classList.add('card');
+        label.innerHTML = card.map((bit, i) => bit ? shapes[i] : '').join('');
+
+        cardWrapper.appendChild(checkbox);
+        cardWrapper.appendChild(label);
+        cardsContainer.appendChild(cardWrapper);
     });
 
     checkButton.addEventListener('click', () => {
-        // Gather selected cards
-        const selectedCards = [...document.querySelectorAll('.selected')].map(el =>
-            cards[el.dataset.index]
-        );
+        // Gather selected cards based on checkboxes
+        const selectedCardsIndices = [...document.querySelectorAll('.card-checkbox:checked')].map(cb => cb.id.replace('card-', ''));
+        const selectedCards = selectedCardsIndices.map(index => cards[index]);
 
         // Sum columns of selected cards
         const sums = selectedCards.reduce((acc, card) => {
